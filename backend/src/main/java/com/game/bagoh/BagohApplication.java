b138a6ebbb56a3cc4ab722fd089253215f39b5b7
Nairon <galveznairon@gmail.com>
2022-06-07 22:07:50 -0300

    First Commit Nairon

---
+++ backend/src/main/java/com/game/bagoh/BagohApplication.java
@@ -0,0 +1,13 @@
+package com.game.bagoh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BagohApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BagohApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/game/bagoh/BagohApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.bagoh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BagohApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
